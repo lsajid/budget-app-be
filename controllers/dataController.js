e08ed5eddd6d29725c2d80c8f932b92146101a49
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-20 23:06:33 -0500

    PUT request working and confirmed via POSTMAN

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -11,14 +11,17 @@ dataRoute.use((req, res, next) => {
 dataRoute.get("/", (req, res) => {
     res.json(dataArray);
 });
+
 //GET :/index
 dataRoute.get("/:index", (req, res) => {
     const { index } = req.params;
      if (dataArray[index]) {
          res.send(dataArray[index]);
      }else {
-         res.redirect("/");
-     }
+        res.status(404).json({ 
+            error: "Index not found"
+        });
+     };
 });
 
 //POST
@@ -27,11 +30,39 @@ dataRoute.post("/",(req,res) => {
     res.json(dataArray[dataArray.length-1]);
 });
 
-//DELETE /:index
+//DELETE /:index returns array
 dataRoute.delete("/:index", (req, res) => {
-    //
+    const { index } = req.params;
+
+    if(dataArray[index]) {
+        let remove = dataArray.splice(dataArray[index], 1);
+        res.json(remove);
+    } else {
+        res.status(404).json({error: "Not found"})
+    };
 });
 
-//PUT
+//PUT /:index
+dataRoute.put("/:index", (req, res) => {
+    const { index } = req.params;
+    let { date, name, amount, from } = req.body;
+    
+    if(!dataArray[index]) {
+        res.status(404).json({
+            error: "Index DNE"
+        });
+        return;
+    }
+    if( date && name && amount && from ) {
+        dataArray[index] = {
+            date, name, amount, from
+        };
+        res.json(dataArray[index]);
+    } else {
+        res.status(422).json({
+            error: "Put Request not completed"
+        });
+    }
+});
 
 module.exports = dataRoute;
