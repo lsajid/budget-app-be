f69ad174f2c364b01205c9699390a91a040eb557
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-22 03:12:49 -0500

    changed /data route to /transactions; used better naming convention

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
-const dataController = require("./controllers/dataController.js");
+const transactionsController = require("./controllers/transactionsController.js");
 
 app.use(express.json());
 app.use(cors());
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use("/data", dataController );
+app.use("/transactions", transactionsController );
 
 app.get("/", (req, res) => {
     res.send(`Budget-App`);
---
+++ controllers/transactionsController.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const transactionsRoute = express.Router();
+const transactionsArr = require("../models/data");
+
+transactionsRoute.use((req, res, next) => {
+    console.log(`Trigger 2: .use within controller`);
+    next();
+});
+
+//GET all
+transactionsRoute.get("/", (req, res) => {
+    res.json(transactionsArr);
+});
+
+//GET :/index
+transactionsRoute.get("/:index", (req, res) => {
+    const { index } = req.params;
+     if (transactionsArr[index]) {
+         res.send(transactionsArr[index]);
+     }else {
+        res.status(404).json({ 
+            error: "Index not found"
+        });
+     };
+});
+
+//POST
+transactionsRoute.post("/",(req,res) => {
+    transactionsArr.push(req.body);
+    res.json(transactionsArr[transactionsArr.length-1]);
+});
+
+//DELETE /:index returns array
+transactionsRoute.delete("/:index", (req, res) => {
+    const { index } = req.params;
+
+    if(transactionsArr[index]) {
+        let remove = transactionsArr.splice(transactionsArr[index], 1);
+        res.json(remove);
+    } else {
+        res.status(404).json({error: "Not found"})
+    };
+});
+
+//PUT /:index
+transactionsRoute.put("/:index", (req, res) => {
+    const { index } = req.params;
+    let { date, name, amount, from } = req.body;
+    
+    if(!transactionsArr[index]) {
+        res.status(404).json({
+            error: "Index DNE"
+        });
+        return;
+    }
+    if( date && name && amount && from ) {
+        transactionsArr[index] = {
+            date, name, amount, from
+        };
+        res.json(transactionsArr[index]);
+    } else {
+        res.status(422).json({
+            error: "Put Request not completed"
+        });
+    }
+});
+
+module.exports = transactionsRoute;
